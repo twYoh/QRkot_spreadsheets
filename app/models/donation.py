b1d7c904fe5d8ee88bc043c159582b3de0e7f9ab
Rich <rsyohasakura@ya.ru>
2025-12-01 10:59:20 +0300

    fix models

--- app/models/donation.py
+++ app/models/donation.py
@@ -5,5 +5,13 @@ from app.models.base import FinanceBaseModel
 
 class Donation(FinanceBaseModel):
     """Модель пожертвований."""
-    user_id = Column(Integer, ForeignKey('user.id'))
-    comment = Column(Text)
+    __tablename__ = "donation"
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    comment = Column(Text, nullable=True)
+
+    def __repr__(self):
+        return (
+            f"{super().__repr__()}, "
+            f"user_id={self.user_id}, "
+            f"comment='{self.comment}')"
+        )
