82627493a08e5c92e220355f993b209a0e985832
Rich <rsyohasakura@ya.ru>
2025-12-01 11:02:25 +0300

    fix models v2.0

--- app/models/base.py
+++ app/models/base.py
@@ -1,19 +1,40 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, Integer
+from sqlalchemy import Boolean, Column, DateTime, Integer,  CheckConstraint
 
 from app.core.db import Base
 from app.core.constants import DEFAULT_VALUE
 
 
 class FinanceBaseModel(Base):
-    """
-    Базовая абстрактная модель,
-    используемая как основа для проектов и пожертвований.
-    """
+    """Абстрактная базовая модель для пожертвований и проектов."""
+
     __abstract__ = True
+
     full_amount = Column(Integer, nullable=False)
     invested_amount = Column(Integer, default=DEFAULT_VALUE)
     fully_invested = Column(Boolean(), default=False)
     create_date = Column(DateTime, default=datetime.now)
     close_date = Column(DateTime)
+
+    __table_args__ = (
+        CheckConstraint(
+            'full_amount > 0',
+            name='check_full_amount_positive'
+        ),
+        CheckConstraint(
+            '0 <= invested_amount <= full_amount',
+            name='check_invested_valid_range'
+        ),
+    )
+
+    def __str__(self):
+        return (
+            f"{type(self).__name__}("
+            f"{self.full_amount=}, "
+            f"{self.invested_amount=}, "
+            f"{self.fully_invested=}, "
+            f"{self.create_date=}, "
+            f"{self.close_date=}"
+            ")"
+        )
--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -5,6 +5,13 @@ from app.core.constants import MAX_LENGTH
 
 
 class CharityProject(FinanceBaseModel):
-    """Модель проектов."""
+    __tablename__ = "charityproject"
     name = Column(String(MAX_LENGTH), unique=True, nullable=False)
-    description = Column(Text())
+    description = Column(Text, nullable=False)
+
+    def __repr__(self):
+        return (
+            f"{super().__repr__()}, "
+            f"name={self.name}, "
+            f"description={self.description})"
+        )
